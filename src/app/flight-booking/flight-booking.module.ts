462b712b7a4ac824217c2a4aab9617f50ddfa1eb
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-04-20 11:23:33 +0200

    [RF] Edit flights

--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -13,6 +13,7 @@ import { CityValidatorDirective } from './shared/validation/city-validator.direc
 import { AsyncCityValidatorDirective } from './shared/validation/async-city-validator.directive';
 import { MultiFieldValidatorDirective } from './shared/validation/multi-field-validator.directive';
 import { AsyncMultiFieldValidatorDirective } from './shared/validation/async-multi-field-validator.directive';
+import { FlightEditComponent } from './flight-edit/flight-edit.component';
 
 @NgModule({
   imports: [SharedModule],
@@ -27,7 +28,8 @@ import { AsyncMultiFieldValidatorDirective } from './shared/validation/async-mul
     CityValidatorDirective,
     AsyncCityValidatorDirective,
     MultiFieldValidatorDirective,
-    AsyncMultiFieldValidatorDirective
+    AsyncMultiFieldValidatorDirective,
+    FlightEditComponent
   ],
   exports: [SharedModule, FlightSearchComponent, FlightLookaheadComponent, AirportsComponent, BookingHistoryComponent]
 })
