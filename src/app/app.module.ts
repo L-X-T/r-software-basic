96ae5c85cb27d9b0c75cb7c68824d3056f043042
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-09-12 11:32:24 +0200

    Standalone component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FlightBookingModule } from './flight-booking/flight-booking.module';
+import { FlightSearchComponent } from './components/flight-search/flight-search.component';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, FlightBookingModule],
+  imports: [BrowserModule, HttpClientModule, FlightBookingModule, FlightSearchComponent],
   declarations: [AppComponent, SidebarComponent, NavbarComponent],
   bootstrap: [AppComponent]
 })
--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
-import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { FlightLookaheadComponent } from './flight-lookahead/flight-lookahead.component';
 import { AirportsComponent } from './airports/airports.component';
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [FlightSearchComponent, FlightLookaheadComponent, AirportsComponent],
-  exports: [SharedModule, FlightSearchComponent, FlightLookaheadComponent, AirportsComponent]
+  declarations: [FlightLookaheadComponent, AirportsComponent],
+  exports: [SharedModule, FlightLookaheadComponent, AirportsComponent]
 })
 export class FlightBookingModule {}
