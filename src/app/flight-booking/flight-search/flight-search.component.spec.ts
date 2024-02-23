66fd6b72bd4de6de543028aa521d4a7f89f7327b
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-02-23 19:13:35 +0100

    [UT] Unit-Test for a Component

--- src/app/flight-booking/flight-search/flight-search.component.spec.ts
+++ src/app/flight-booking/flight-search/flight-search.component.spec.ts
@@ -1,31 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
-import { SharedModule } from '../../shared/shared.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { FlightBookingModule } from '../flight-booking.module';
+import { SharedModule } from '../../shared/shared.module';
 
 import { FlightSearchComponent } from './flight-search.component';
 
-describe('FlightSearchComponent', () => {
+describe('Unit test: flight-search.component', () => {
   let component: FlightSearchComponent;
   let fixture: ComponentFixture<FlightSearchComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, FlightBookingModule, SharedModule],
-      declarations: [FlightSearchComponent]
-    }).compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule, FlightBookingModule, SharedModule],
+      providers: [
+        // Add Providers if you need them for your tests
+      ]
+    });
+
     fixture = TestBed.createComponent(FlightSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should not have any flights loaded initially', () => {
+    expect(component.flights.length).toBe(0);
   });
 });
