981bd0ac624d932893fe677053dd2a6ed22f0212
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-02-28 11:58:05 +0100

    HTTP Client: Listen to response demo

--- src/app/flight-search/flight.service.ts
+++ src/app/flight-search/flight.service.ts
@@ -1,9 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, of, switchMap, throwError } from 'rxjs';
 
 import { Flight } from '../entities/flight';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class FlightService {
@@ -16,7 +18,25 @@ export class FlightService {
 
   find(from: string, to: string): Observable<Flight[]> {
     const params = new HttpParams().set('from', from).set('to', to);
-    return this.http.get<Flight[]>(this.url, { headers: this.headers, params });
+    return this.http.get<Flight[]>(this.url, { headers: this.headers, params, observe: 'response' }).pipe(
+      switchMap((response) => {
+        if (!environment.production) {
+          console.debug('response status code:', response.status);
+        }
+
+        // replace with real status
+        if (response.status === 123) {
+          return of([]); // empty response
+        }
+
+        // replace with real error
+        if (response.status === 456) {
+          return throwError(() => new Error('This is an error message!'));
+        }
+
+        return of(response.body as Flight[]);
+      })
+    );
   }
 
   save(flight: Flight): Observable<Flight> {
