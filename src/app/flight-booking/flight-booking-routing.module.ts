43232c92e6a02c984e35612eb14ba08ffccb1c9c
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-03-01 15:09:28 +0100

    [Routing] Add additional routes for other components

--- src/app/flight-booking/flight-booking-routing.module.ts
+++ src/app/flight-booking/flight-booking-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
 import { FlightEditComponent } from './flight-edit/flight-edit.component';
+import { FlightLookaheadComponent } from './flight-lookahead/flight-lookahead.component';
 
 const flightBookingRoutes: Routes = [
   {
@@ -16,6 +17,10 @@ const flightBookingRoutes: Routes = [
   {
     path: 'passenger-search',
     component: PassengerSearchComponent
+  },
+  {
+    path: 'flight-lookahead',
+    component: FlightLookaheadComponent
   }
 ];
 
